6939e9106a92a9222f7d594b69c1ce958597f234
EstherMutua <esthermutua16@gmail.com>
2025-03-07 11:09:36 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickUpLocation) {
+    const headquarters = 42;
+    return Math.abs(pickUpLocation - headquarters);
+}
+
+function distanceFromHqInFeet(pickUpLocation) {
+    return distanceFromHqInBlocks(pickUpLocation) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(destination - start) * 264;
+}
+
+function calculatesFarePrice(start, destination){
+    const distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400){
+        return 0;
+    }
+    else if(distance > 400 && distance <= 2000 ){
+        return (distance- 400) * 0.02;
+    }
+    else if(distance > 2000 && distance <= 2500){
+        return 25;
+    }
+    else{
+        return "cannot travel that far";
+    }
+}
